d28ac7563cbb1e19d326d664870f725fa807e865
Ricsi <allili.hamid@gmail.com>
2025-09-04 18:04:23 +0200

    feat(backend): API for chatting with LLM
    - LLM is OpenAI

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,9 +1,12 @@
 import express, { type Request, type Response } from 'express'
+import { chatController } from './controllers/chat.controller';
 
 const router = express.Router()
 
 router.get('/', (req: Request, res: Response): void => {
   res.json({ message: 'Hello World!' })
 })
+
+router.post('/api/chat', chatController.sendMessage)
 
 export default router
