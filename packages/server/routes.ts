ee0fceb63b9514c87f9a1e7f09898307e506c6c4
Ricsi <allili.hamid@gmail.com>
2025-09-05 22:39:49 +0200

    feat(backend): API to fetch Reviews for a product

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from 'express';
 import { chatController } from './controllers/chat.controller';
+import { PrismaClient } from './generated/prisma';
 
 const router = express.Router();
 
@@ -8,5 +9,25 @@ router.get('/api/hello', (req: Request, res: Response): void => {
 });
 
 router.post('/api/chat', chatController.sendMessage);
+
+router.get(
+  '/api/products/:id/reviews/',
+  async (req: Request, res: Response) => {
+    const prisma = new PrismaClient();
+    const productId = Number(req.params.id);
+
+    if (isNaN(productId)) {
+      res.status(400).json({ error: 'Invalid product ID. ' });
+      return;
+    }
+
+    const reviews = await prisma.review.findMany({
+      where: { productId },
+      orderBy: { createdAt: 'desc' },
+    });
+
+    res.json(reviews);
+  }
+);
 
 export default router;
