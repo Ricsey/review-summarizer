1a830c0bac48152b93d7c566475efaf91c1c512c
Ricsi <allili.hamid@gmail.com>
2025-09-06 10:18:26 +0200

    feat(summary): dummy API for summarizing review
    - preparing summarization API with dummy summary response

--- packages/server/routes.ts
+++ packages/server/routes.ts
@@ -2,6 +2,7 @@ import { type Request, type Response } from 'express';
 import express from 'express';
 import { chatController } from './controllers/chat.controller';
 import { reviewController } from './controllers/review.controller';
+import { summarizeController } from './controllers/summarize.controller';
 
 const router = express.Router();
 
@@ -12,5 +13,10 @@ router.get('/api/hello', (req: Request, res: Response): void => {
 router.post('/api/chat', chatController.sendMessage);
 
 router.get('/api/products/:id/reviews/', reviewController.getReviews);
+
+router.post(
+  '/api/products/:id/reviews/summarize/',
+  reviewController.summarizeReviews
+);
 
 export default router;
